88ba598194050c0e8224069b6a12f0312365faac
lajohnston <lajohnston@live.co.uk>
2025-02-20 21:51:08 +0000

    Refactor LevelSerializer tests

--- LemballEditor.Tests/Serializers/LevelSerializerTests.cs
+++ LemballEditor.Tests/Serializers/LevelSerializerTests.cs
@@ -9,73 +9,81 @@ namespace LemballEditor.Tests.Serializers
     [TestClass]
     public sealed class LevelSerializerTests
     {
-        [TestMethod]
-        public void Deserialize_ShouldThrowAnInvalidDataException_WhenHeaderIsNotValid()
-        {
-            var invalidHeader = Encoding.UTF8.GetBytes("UNEXPECTED HEADER BYTES");
-
-            using var stream = new MemoryStream(invalidHeader);
-            using var reader = new BinaryReader(stream);
-
-            var level = new Level();
-            var act = () => LevelSerializer.Deserialize(level, reader);
-
-            _ = act.Should().Throw<InvalidDataException>("Invalid header");
-        }
-
-        [TestMethod]
-        public void Deserialize_ShouldThrowAnExceptionIfTheUnknownAValueIsInvalid()
-        {
-            var fakeErrorMessage = "Some Error";
-
-            // Mock a level that throws an exception when UnknownA is set
-            var mockLevel = new Mock<ILevel>();
-            _ = mockLevel.SetupSet(m => m.UnknownA = It.IsAny<ushort>()).Throws<ArgumentException>();
-
-            using var stream = new MemoryStream();
-            using var reader = new BinaryReader(stream);
-
-            // Serialize valid data
-            using var writer = new BinaryWriter(stream);
-            LevelSerializer.Serialize(mockLevel.Object, writer);
-
-            // Deserialize the data
-            _ = stream.Seek(0, SeekOrigin.Begin);
-            var act = () => LevelSerializer.Deserialize(mockLevel.Object, reader);
-
-            _ = act.Should().Throw<InvalidDataException>(fakeErrorMessage);
-        }
-
-        [TestMethod]
-        public void ShouldGetAndSetUnknownA()
+        private ILevel serializeAndDeserialize(ILevel level)
         {
             using var stream = new MemoryStream();
             using var reader = new BinaryReader(stream);
 
             // Serialize data
             using var writer = new BinaryWriter(stream);
-            LevelSerializer.Serialize(new Level
-            {
-                UnknownA = 10
-            }, writer);
+            LevelSerializer.Serialize(level, writer);
 
             // Deserialize the data
             _ = stream.Seek(0, SeekOrigin.Begin);
             var resultLevel = new Level();
             LevelSerializer.Deserialize(resultLevel, reader);
 
-            _ = resultLevel.UnknownA.Should().Be(10);
+            return resultLevel;
+        }
+
+        private byte[] getSerializedData(ILevel level)
+        {
+            using var stream = new MemoryStream();
+            using var writer = new BinaryWriter(stream);
+
+            LevelSerializer.Serialize(level, writer);
+
+            return stream.ToArray();
+        }
+
+        private void deserialize(byte[] data, ILevel level)
+        {
+            using var stream = new MemoryStream(data);
+            using var reader = new BinaryReader(stream);
+
+            LevelSerializer.Deserialize(level, reader);
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnInvalidDataException_WhenHeaderIsNotValid()
+        {
+            var invalidHeader = Encoding.UTF8.GetBytes("UNEXPECTED HEADER BYTES");
+            var act = () => deserialize(invalidHeader, new Level());
+            _ = act.Should().Throw<InvalidDataException>("Invalid header");
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnExceptionIfTheUnknownAValueIsInvalid()
+        {
+            // Mock a level that throws an exception when UnknownA is set
+            var fakeErrorMessage = "Some Error";
+            var mockLevel = new Mock<ILevel>();
+            _ = mockLevel.SetupSet(m => m.UnknownA = It.IsAny<ushort>()).Throws<ArgumentException>();
+
+            var act = () => serializeAndDeserialize(mockLevel.Object);
+            _ = act.Should().Throw<InvalidDataException>(fakeErrorMessage);
+        }
+
+        [TestMethod]
+        public void ShouldGetAndSetUnknownA()
+        {
+            var level = new Level()
+            {
+                UnknownA = 10
+            };
+
+            var result = serializeAndDeserialize(level);
+            _ = result.UnknownA.Should().Be(10);
         }
 
         [TestMethod]
         public void Serialize_ShouldWriteTheCorrectHeaderBytes()
         {
-            using var stream = new MemoryStream();
-            using var writer = new BinaryWriter(stream);
-            using var reader = new BinaryReader(stream);
-
             var level = new Level();
-            LevelSerializer.Serialize(level, writer);
+            var data = getSerializedData(level);
+
+            using var stream = new MemoryStream(data);
+            using var reader = new BinaryReader(stream);
 
             byte[] validHeader = { 0x20, 0x20, 0x49, 0x41, 0x12, 0x0, 0x0, 0x0 };
 
