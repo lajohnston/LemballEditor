4ed0c68dcc02d1bcb86e25feba3fba31e108497b
lajohnston <lajohnston@live.co.uk>
2025-02-23 16:30:26 +0000

    Add Tile model

---
+++ LemballEditor.Tests/ModelTests/TileTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using LemballEditor.Models;
+
+namespace LemballEditor.Tests.ModelTests
+{
+    [TestClass]
+    public class TileTests
+    {
+        [TestMethod]
+        public void ShouldInitialiseWithTheTileRef()
+        {
+            ushort tileRef = 123;
+            var tile = new Tile(tileRef);
+            _ = tile.TileRef.Should().Be(123);
+        }
+
+        [TestMethod]
+        public void ShouldGetAndSetTheTileRef()
+        {
+            var tile = new Tile(0)
+            {
+                TileRef = 123
+            };
+            _ = tile.TileRef.Should().Be(123);
+        }
+
+        [TestMethod]
+        public void ShouldGetAndSetTheElevation()
+        {
+            var tile = new Tile(0)
+            {
+                Elevation = 88
+            };
+            _ = tile.Elevation.Should().Be(88);
+        }
+
+        [TestMethod]
+        public void Elevation_ShouldThrowAnException_WhenTheValueIsOver88()
+        {
+            var tile = new Tile(0);
+
+            var act = () => tile.Elevation = 89;
+
+            _ = act.Should().Throw<ArgumentException>().WithMessage("Max elevation is 88");
+        }
+    }
+}
---
+++ src/Models/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LemballEditor.Models
+{
+    /// <summary>
+    /// A tile in the level map
+    /// </summary>
+    public class Tile
+    {
+        /// <summary>
+        /// The tile pattern reference
+        /// </summary>
+        public ushort TileRef { get; set; }
+
+        /// <summary>
+        /// The elevation of the tile in pixels. Max value is 88 before the graphics get cropped
+        /// </summary>
+        public byte Elevation
+        {
+            get => _elevation;
+            set
+            {
+                if (value > 88)
+                {
+                    throw new ArgumentException("Max elevation is 88");
+                }
+
+                _elevation = value;
+            }
+        }
+        private byte _elevation;
+
+        /// <summary>
+        /// Creates a new tile with the given tileRef
+        /// </summary>
+        /// <param name="tileRef">The tile pattern reference</param>
+        public Tile(ushort tileRef)
+        {
+            TileRef = tileRef;
+            Elevation = 0;
+        }
+    }
+}
