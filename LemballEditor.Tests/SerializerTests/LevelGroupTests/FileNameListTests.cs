b663fafacf8491d0e0dcdaed0a5fe53b188da389
lajohnston <lajohnston@live.co.uk>
2025-03-14 16:01:30 +0000

    Add FileNameList serializer/deserializer

---
+++ LemballEditor.Tests/SerializerTests/LevelGroupTests/FileNameListTests.cs
@@ -0,0 +1,182 @@
+﻿using FluentAssertions;
+using LemballEditor.Serializers.LevelGroup;
+using System.Text;
+
+namespace LemballEditor.Tests.SerializerTests.LevelGroupTests
+{
+    [TestClass]
+    public class FileNameListTests
+    {
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnException_WhenTheLevelsNamesAreNotValid()
+        {
+            byte numberOfLevels = 2;
+            var invalidName = "Lev_01";
+
+            var data = new List<byte>();
+            data.AddRange(Encoding.ASCII.GetBytes("Level_00"));
+            data.AddRange(new byte[4]); // padding
+
+            data.AddRange(Encoding.ASCII.GetBytes(invalidName));
+            data.AddRange(new byte[4]); // padding
+
+            using var stream = new MemoryStream(data.ToArray());
+            using var reader = new BinaryReader(stream);
+
+            var group = new PendingLevelGroup
+            {
+                LevelCount = numberOfLevels
+            };
+
+            var serializer = new FileNameList();
+            var act = () => serializer.Deserialize(reader, group);
+
+            _ = act.Should().Throw<InvalidDataException>().WithMessage("Invalid file name");
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnException_WhenTheLevelsNamesAreNotFollowedByEightZeros()
+        {
+            byte numberOfLevels = 2;
+
+            var data = new List<byte>();
+            data.AddRange(Encoding.ASCII.GetBytes("Level_00"));
+            data.AddRange(new byte[4]); // padding
+
+            data.AddRange(Encoding.ASCII.GetBytes("Level_01"));
+            data.AddRange(new byte[] { 0, 0, 1, 0 }); // invalid padding
+
+            using var stream = new MemoryStream(data.ToArray());
+            using var reader = new BinaryReader(stream);
+
+            var group = new PendingLevelGroup
+            {
+                LevelCount = numberOfLevels
+            };
+
+            var serializer = new FileNameList();
+            var act = () => serializer.Deserialize(reader, group);
+
+            _ = act.Should().Throw<InvalidDataException>().WithMessage("Invalid data found after file name");
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnException_WhenTheFirstLevelIsNotLevel00()
+        {
+            byte numberOfLevels = 1;
+
+            var data = new List<byte>();
+            data.AddRange(Encoding.ASCII.GetBytes("Level_01"));
+            data.AddRange(new byte[4]); // padding
+
+            using var stream = new MemoryStream(data.ToArray());
+            using var reader = new BinaryReader(stream);
+
+            var group = new PendingLevelGroup
+            {
+                LevelCount = numberOfLevels
+            };
+
+            var serializer = new FileNameList();
+            var act = () => serializer.Deserialize(reader, group);
+
+            _ = act.Should().Throw<InvalidDataException>().WithMessage("Invalid file name");
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnException_WhenTheLevelNamesArNotSequential()
+        {
+            byte numberOfLevels = 2;
+
+            var data = new List<byte>();
+            data.AddRange(Encoding.ASCII.GetBytes("Level_00"));
+            data.AddRange(new byte[4]); // padding
+
+            data.AddRange(Encoding.ASCII.GetBytes("Level_02"));
+            data.AddRange(new byte[4]); // padding
+
+            using var stream = new MemoryStream(data.ToArray());
+            using var reader = new BinaryReader(stream);
+
+            var group = new PendingLevelGroup
+            {
+                LevelCount = numberOfLevels
+            };
+
+            var serializer = new FileNameList();
+            var act = () => serializer.Deserialize(reader, group);
+
+            _ = act.Should().Throw<InvalidDataException>().WithMessage("Invalid file name");
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldSkipTheFileNameBytesAndReturnTheGivenModel_WhenTheDataIsValid()
+        {
+            byte numberOfLevels = 2;
+
+            var data = new List<byte>();
+            data.AddRange(Encoding.ASCII.GetBytes("Level_00"));
+            data.AddRange(new byte[4]); // padding
+
+            data.AddRange(Encoding.ASCII.GetBytes("Level_01"));
+            data.AddRange(new byte[4]); // padding
+
+            using var stream = new MemoryStream(data.ToArray());
+            using var reader = new BinaryReader(stream);
+
+            var group = new PendingLevelGroup
+            {
+                LevelCount = numberOfLevels
+            };
+
+            var serializer = new FileNameList();
+            _ = serializer.Deserialize(reader, group).Should().Be(group);
+
+            _ = stream.Position.Should().Be(numberOfLevels * 12);
+        }
+
+        [TestMethod]
+        public void Serialize_WriteTheSequentialFilesNameWithFourBytesOfPadding()
+        {
+            byte numberOfLevels = 11;
+
+            using var stream = new MemoryStream();
+            using var writer = new BinaryWriter(stream);
+            using var reader = new BinaryReader(stream);
+
+            var group = new PendingLevelGroup
+            {
+                LevelCount = numberOfLevels
+            };
+
+            var expected = new List<byte>();
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_00"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_01"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_02"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_03"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_04"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_05"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_06"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_07"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_08"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_09"));
+            expected.AddRange(new byte[4]);
+            expected.AddRange(Encoding.ASCII.GetBytes("Level_10"));
+            expected.AddRange(new byte[4]);
+
+            var serializer = new FileNameList();
+            serializer.Serialize(group, writer);
+
+            _ = stream.ToArray().Should().BeEquivalentTo(expected.ToArray());
+        }
+    }
+}
---
+++ src/Serializers/LevelGroup/FileNameList.cs
@@ -0,0 +1,50 @@
+﻿using LemballEditor.Serializers.Level;
+using System.IO;
+using System.Text;
+
+namespace LemballEditor.Serializers.LevelGroup
+{
+    /// <summary>
+    /// Serializes/Deserializes the level file names (not the in-game level titles)
+    /// </summary>
+    public class FileNameList : ISerializer<PendingLevelGroup>
+    {
+        /// <summary>
+        /// Validates the file name list and returns the given mode if it's valid
+        /// </summary>
+        /// <exception cref="InvalidDataException">If the file name list contains unexpected data</exception>
+        public PendingLevelGroup Deserialize(BinaryReader reader, PendingLevelGroup model)
+        {
+            for (var levelNumber = 0; levelNumber < model.LevelCount; levelNumber++)
+            {
+                var name = Encoding.ASCII.GetString(reader.ReadBytes(8));
+
+                if (name != "Level_" + levelNumber.ToString("00"))
+                {
+                    throw new InvalidDataException("Invalid file name");
+                }
+
+                var padding = reader.ReadUInt32();
+
+                if (padding != 0)
+                {
+                    throw new InvalidDataException("Invalid data found after file name");
+                }
+            }
+
+            return model;
+        }
+
+        /// <summary>
+        /// Writes the sequential file name list to the stream
+        /// </summary>
+        public void Serialize(PendingLevelGroup model, BinaryWriter writer)
+        {
+            for (var levelNumber = 0; levelNumber < model.LevelCount; levelNumber++)
+            {
+                writer.Write(Encoding.ASCII.GetBytes("Level_" + levelNumber.ToString("00")));
+                writer.Write(new byte[4]);
+            }
+        }
+    }
+}
