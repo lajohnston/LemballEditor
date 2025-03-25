5083f42ac31245bd144e3dba4581dedd7e13c3c2
lajohnston <lajohnston@live.co.uk>
2025-03-25 18:35:21 +0000

    Add AddressToFileDescriptors serializer

---
+++ LemballEditor.Tests/SerializerTests/LevelGroupTests/AddressToFileDescriptorsTests.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+using LemballEditor.Serializers.LevelDirectory;
+
+namespace LemballEditor.Tests.SerializerTests.LevelGroupTests
+{
+    [TestClass]
+    public class AddressToFileDescriptorsTests
+    {
+        [TestMethod]
+        public void Deserialize_ShouldThrowAnInvalidDataExceptionIfTheValueIsNotValid()
+        {
+            var serializer = new AddressToFileDescriptors();
+            var directory = new LevelDirectory()
+            {
+                DirectoryAddress = 20000,
+                FixedLevelCount = 5
+            };
+
+            var expected = directory.DirectoryAddress + 16 + (uint)(directory.FixedLevelCount * 12);
+            var invalidValue = expected + 1;
+
+            var data = BitConverter.GetBytes(invalidValue);
+
+            using var stream = new MemoryStream(data);
+            using var reader = new BinaryReader(stream);
+
+            var act = () => serializer.Deserialize(reader, directory);
+
+            _ = act.Should().Throw<InvalidDataException>().WithMessage($"File descriptor address invalid. Expected {expected} not {invalidValue}");
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldReadPastThePointer_WhenTheValidIsValid()
+        {
+            var serializer = new AddressToFileDescriptors();
+            var directory = new LevelDirectory()
+            {
+                DirectoryAddress = 20000,
+                FixedLevelCount = 5
+            };
+
+            var address = directory.DirectoryAddress + 16 + (uint)(directory.FixedLevelCount * 12);
+            var data = BitConverter.GetBytes(address);
+
+            using var stream = new MemoryStream(data);
+            using var reader = new BinaryReader(stream);
+
+            _ = serializer.Deserialize(reader, directory);
+            _ = stream.Position.Should().Be(4);
+        }
+
+        [TestMethod]
+        public void Deserialize_ShouldReturnTheDirectory_WhenTheValueIsValid()
+        {
+            var serializer = new AddressToFileDescriptors();
+            var directory = new LevelDirectory()
+            {
+                DirectoryAddress = 50000,
+                FixedLevelCount = 4
+            };
+
+            var address = directory.DirectoryAddress + 16 + (uint)(directory.FixedLevelCount * 12);
+            var data = BitConverter.GetBytes(address);
+
+            using var stream = new MemoryStream(data);
+            using var reader = new BinaryReader(stream);
+
+            _ = serializer.Deserialize(reader, directory).Should().Be(directory);
+        }
+
+        [TestMethod]
+        public void Serialize_ShouldWriteTheAddressBasedOnTheLevelCountAndBaseAddress()
+        {
+            var serializer = new AddressToFileDescriptors();
+            var directory = new LevelDirectory
+            {
+                DirectoryAddress = 1000,
+                FixedLevelCount = 10
+            };
+
+            using var stream = new MemoryStream();
+            using var writer = new BinaryWriter(stream);
+
+            serializer.Serialize(directory, writer);
+            _ = BitConverter.ToUInt32(stream.ToArray(), 0).Should().Be(directory.DirectoryAddress + 16 + (uint)(directory.FixedLevelCount * 12));
+        }
+    }
+}
---
+++ src/Serializers/LevelDirectory/AddressToFileDescriptors.cs
@@ -0,0 +1,37 @@
+﻿using LemballEditor.Serializers.Level;
+using System.IO;
+
+namespace LemballEditor.Serializers.LevelDirectory
+{
+    /// <summary>
+    /// The address of the file descriptors in the directory, bypassing the file names.
+    /// </summary>
+    public class AddressToFileDescriptors : ISerializer<LevelDirectory>
+    {
+        private static readonly uint HEADER_SIZE = 16;
+        private static readonly uint FILE_NAME_SIZE = 12;
+
+        private uint CalculateValue(LevelDirectory directory)
+        {
+            return directory.DirectoryAddress + HEADER_SIZE + (directory.FixedLevelCount * FILE_NAME_SIZE);
+        }
+
+        public LevelDirectory Deserialize(BinaryReader reader, LevelDirectory levelDirectory)
+        {
+            var value = reader.ReadUInt32();
+            var expected = CalculateValue(levelDirectory);
+
+            if (value != expected)
+            {
+                throw new InvalidDataException($"File descriptor address invalid. Expected {expected} not {value}");
+            }
+
+            return levelDirectory;
+        }
+
+        public void Serialize(LevelDirectory levelDirectory, BinaryWriter writer)
+        {
+            writer.Write(CalculateValue(levelDirectory));
+        }
+    }
+}
