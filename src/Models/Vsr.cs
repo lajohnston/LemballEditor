86a377806e4d5ccb6b367cbdcbfa7bfe14149c05
lajohnston <lajohnston@live.co.uk>
2025-04-07 20:17:12 +0100

    Add Get and SetLevelDirectoryData to VSR model

--- src/Models/Vsr.cs
+++ src/Models/Vsr.cs
@@ -8,9 +8,19 @@ namespace LemballEditor.Models
     public class Vsr
     {
         /// <summary>
-        /// The static asset data, excluding the level directories at the end
+        /// The static asset data, excluding the level directories
         /// </summary>
         public byte[] AssetData { get; set; }
+
+        /// <summary>
+        /// The raw level directory data
+        /// </summary>
+        //public byte[] LevelData { get; set; }
+
+        /// <summary>
+        /// The level directory data, indexed by level group name
+        /// </summary>
+        private readonly Dictionary<LevelGroupName, byte[]> levelDirectoryData;
 
         /// <summary>
         /// The pointers to the level directories within the VSR
@@ -27,6 +37,8 @@ namespace LemballEditor.Models
                 { LevelGroupName.Mayhem, 0 },
                 { LevelGroupName.Network, 0 }
             };
+
+            levelDirectoryData = new Dictionary<LevelGroupName, byte[]>();
         }
 
         /// <summary>
@@ -43,6 +55,26 @@ namespace LemballEditor.Models
 
                 return (uint)AssetData.Length;
             }
+        }
+
+        /// <summary>
+        /// Sets the raw level directory data for the given level group
+        /// </summary>
+        /// <param name="levelGroupName">The level group</param>
+        /// <param name="data">The raw VSR binary data</param>
+        public void SetLevelDirectoryData(LevelGroupName levelGroupName, byte[] data)
+        {
+            this.levelDirectoryData[levelGroupName] = data;
+        }
+
+        /// <summary>
+        /// Retrieves the raw level directory data for the given level group
+        /// </summary>
+        /// <param name="levelGroupName">The level group</param>
+        /// <returns>The raw VSR binary data</returns>
+        public byte[] GetLevelDirectoryData(LevelGroupName levelGroupName)
+        {
+            return this.levelDirectoryData[levelGroupName];
         }
 
         /// <summary>
