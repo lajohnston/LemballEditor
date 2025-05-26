6444152bd0df26f92a85a92bc57f7c9a6c0f4a82
lajohnston <lajohnston@live.co.uk>
2025-05-26 20:34:56 +0100

    Add Vsr/AssetBinary serializer

---
+++ LemballEditor.Tests/SerializerTests/Vsr/AssetBinaryTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using LemballEditor.Serializers.Vsr;
+
+namespace LemballEditor.Tests.SerializerTests.Vsr
+{
+    [TestClass]
+    public class AssetBinaryTests
+    {
+        [TestMethod]
+        [DataRow(744, 1000)]
+        [DataRow(740, 1200)]
+        public void Deserialize_ShouldReadTheAssetDataUpToTheFunDirectoryToTheModel(int funPointer, int funDirectoryAddress)
+        {
+            var vsr = ServiceFactory.CreateVsr();
+            vsr.SetLevelDirectoryPointer(Models.LevelGroupName.Fun, (uint)funPointer);
+
+            var vsrData = new List<byte>();
+            vsrData.AddRange(new byte[funPointer]);                             // Padding to the FUN directory pointer
+            vsrData.AddRange(BitConverter.GetBytes((uint)funDirectoryAddress)); // FUN directory address
+            vsrData.AddRange(new byte[funDirectoryAddress - vsrData.Count]);    // Rest of asset data up to the FUN directory
+
+            using var reader = new BinaryReader(new MemoryStream(vsrData.ToArray()));
+
+            var serializer = new AssetBinary();
+            _ = serializer.Deserialize(reader, vsr);
+
+            _ = vsr.AssetData.Should().BeEquivalentTo(vsrData.Take(funDirectoryAddress));
+            _ = reader.BaseStream.Position.Should().Be(funDirectoryAddress);
+        }
+
+        [TestMethod]
+        public void Serialize_ShouldWriteTheAssetDataToTheStream()
+        {
+            var serializer = new AssetBinary();
+
+            var vsr = ServiceFactory.CreateVsr();
+            vsr.AssetData = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            using var stream = new MemoryStream();
+            using var writer = new BinaryWriter(stream);
+
+            serializer.Serialize(vsr, writer);
+
+            _ = stream.ToArray().Should().BeEquivalentTo(vsr.AssetData);
+        }
+    }
+}
---
+++ src/Serializers/Vsr/AssetBinary.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace LemballEditor.Serializers.Vsr
+{
+    public class AssetBinary : ISerializer<Models.Vsr>
+    {
+        /// <summary>
+        /// Reads the asset data up to the FUN directory pointer and sets it to the model
+        /// </summary>
+        public Models.Vsr Deserialize(BinaryReader reader, Models.Vsr vsr)
+        {
+            var startAddress = reader.BaseStream.Position;
+
+            var funDirectoryPointer = vsr.GetLevelDirectoryPointer(Models.LevelGroupName.Fun);
+            reader.BaseStream.Position = funDirectoryPointer;
+            var funDirectoryAddress = reader.ReadInt32();
+
+            reader.BaseStream.Position = startAddress;
+            var assetData = reader.ReadBytes(funDirectoryAddress);
+
+            vsr.AssetData = assetData;
+
+            return vsr;
+        }
+
+        /// <summary>
+        /// Writes the asset data to the stream
+        /// </summary>
+        public void Serialize(Models.Vsr vsr, BinaryWriter writer)
+        {
+            writer.Write(vsr.AssetData);
+        }
+    }
+}
