8f946923845c1c8f9371c4419f825d006ab54339
lajohnston <lajohnston@live.co.uk>
2025-06-14 13:45:28 +0100

    Refactor Integration tests and add Level integration test

---
+++ LemballEditor.Tests/SerializerTests/IntegrationTests/TestHelper.cs
@@ -0,0 +1,110 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LemballEditor.Tests.SerializerTests.IntegrationTests
+{
+    public class TestHelper
+    {
+        public static string GetFixturePath([CallerFilePath] string? callerFilePath = null)
+        {
+            // Get the directory of the current source file
+            var projectDir = Directory.GetParent(Directory.GetParent(Path.GetDirectoryName(callerFilePath)).ToString());
+
+            return Path.Combine(projectDir.ToString(), "Fixtures");
+        }
+
+        public static Stream GetVsrStream()
+        {
+            var fixturePath = GetFixturePath();
+            var vsrPath = Path.Combine(fixturePath, "PBAIMOG.VSR");
+
+            if (!File.Exists(vsrPath))
+            {
+                Assert.Inconclusive($"VSR fixture not found in path {vsrPath}");
+            }
+
+            return File.OpenRead(vsrPath);
+        }
+
+        /// <summary>
+        /// Iterates through the binary reader to find all occurrences of the given byte sequence. Search bytes must have a length of 4 bytes.
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        private static IEnumerable<long> FindBytes(BinaryReader reader, byte[] searchBytes)
+        {
+            var alignment = 4;
+
+            if (searchBytes.Length != alignment)
+            {
+                throw new ArgumentException($"Search string must have a length of {alignment}");
+            }
+
+            var startPosition = reader.BaseStream.Position;
+
+            while (true)
+            {
+                var chunk = reader.ReadBytes(alignment);
+
+                if (chunk.Length < alignment)
+                {
+                    break; // End of stream
+                }
+
+                if (chunk.SequenceEqual(searchBytes))
+                {
+                    var position = reader.BaseStream.Position;
+                    yield return position - alignment;
+                    reader.BaseStream.Position = position;
+                }
+            }
+
+            reader.BaseStream.Position = startPosition;
+        }
+
+        /// <summary>
+        /// Iterates through each data block in the binary reader that starts with the given header. The header must be 4 bytes long.
+        /// </summary>
+        public static IEnumerable<(byte[], long, int)> GetBlocks(BinaryReader reader, string header)
+        {
+            var searchBytes = Encoding.ASCII.GetBytes(header);
+            var startPosition = reader.BaseStream.Position;
+
+            var index = 0;
+            foreach (var position in FindBytes(reader, searchBytes))
+            {
+                reader.BaseStream.Position = position + 4; // point to size
+                var size = reader.ReadInt32();
+                var paddedSize = (size + 3) / 4 * 4;
+
+                reader.BaseStream.Position = position;      // point to start of block
+                var data = reader.ReadBytes(paddedSize);    // read the block data
+                reader.BaseStream.Position = position;      // restore previous position
+
+                yield return (data, position, index);
+                index++;
+            }
+
+            reader.BaseStream.Position = startPosition;
+        }
+
+        public static IEnumerable<(byte[], long, int)> GetLevelBlocks(BinaryReader reader)
+        {
+            var searchBytes = Encoding.ASCII.GetBytes("  IA");
+            var startPosition = reader.BaseStream.Position;
+
+            var index = 0;
+            foreach (var position in FindBytes(reader, searchBytes))
+            {
+                reader.BaseStream.Position = position - 4;  // point to size
+                var size = reader.ReadInt32();
+                var data = reader.ReadBytes(size);          // read the block data
+                reader.BaseStream.Position = position;      // restore previous position
+
+                yield return (data, position, index);
+                index++;
+            }
+
+            reader.BaseStream.Position = startPosition;
+        }
+    }
+}
---
+++ LemballEditor.Tests/SerializerTests/IntegrationTests/VsrSerializerIntegrationTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+
+namespace LemballEditor.Tests.SerializerTests.IntegrationTests
+{
+    [TestClass]
+    public class VsrSerializerIntegrationTests
+    {
+        [TestMethod]
+        public void ItShouldSerializeAndDeserializeAllBomgBlocksInTheStockVsr()
+        {
+            Assert.Inconclusive("This behaviour is not yet implemented");
+
+            using var sourceVsrStream = TestHelper.GetVsrStream();
+            using var sourceReader = new BinaryReader(sourceVsrStream);
+
+            var serializer = ServiceFactory.CreateBomgBlockSerializer();
+
+            foreach (var (blockData, position, index) in TestHelper.GetBlocks(sourceReader, "BOMG"))
+            {
+                using var blockReader = new BinaryReader(new MemoryStream(blockData));
+
+                var deserializeAct = () => serializer.Deserialize(blockReader, ServiceFactory.CreateLevel(1, 1));
+                var deserializedLevel = deserializeAct.Should().NotThrow($"BOMG block {index} at position {position} should be deserialized").Subject;
+
+                using var blockWriter = new BinaryWriter(new MemoryStream());
+                serializer.Serialize(deserializedLevel, blockWriter);
+
+                blockWriter.BaseStream.Position = 0;
+                var serializedBlock = ((MemoryStream)blockWriter.BaseStream).ToArray();
+
+                _ = serializedBlock.Should().BeEquivalentTo(blockData, $"BOMG block {index} at position {position} should match the original data");
+            }
+        }
+
+        [TestMethod]
+        public void ItShouldDeserializeAndSerializeTheStockVsrLevels()
+        {
+            Assert.Inconclusive("This behaviour is not yet implemented");
+
+            using var sourceVsrStream = TestHelper.GetVsrStream();
+            using var sourceReader = new BinaryReader(sourceVsrStream);
+
+            var serializer = ServiceFactory.CreateLevelSerializer();
+
+            foreach (var (levelBlockData, position, index) in TestHelper.GetLevelBlocks(sourceReader))
+            {
+                using var blockReader = new BinaryReader(new MemoryStream(levelBlockData));
+
+                var deserializeAct = () => serializer.Deserialize(blockReader, ServiceFactory.CreateLevel(1, 1));
+                var deserializedLevel = deserializeAct.Should().NotThrow($"Level block {index} at position {position} should be deserialized").Subject;
+
+                using var blockWriter = new BinaryWriter(new MemoryStream());
+                serializer.Serialize(deserializedLevel, blockWriter);
+
+                blockWriter.BaseStream.Position = 0;
+                var serializedBlock = ((MemoryStream)blockWriter.BaseStream).ToArray();
+
+                _ = serializedBlock.Length.Should().Be(
+                    levelBlockData.Length,
+                    $"Level block {index} at position {position} should match the original size"
+                );
+
+                _ = serializedBlock.Should().BeEquivalentTo(
+                    levelBlockData,
+                    $"Level block {index} at position {position} should match the original data"
+                );
+            }
+        }
+
+        [TestMethod]
+        public void ItShouldDeserializeAndSerializeTheStockVsr()
+        {
+            //Assert.Inconclusive("This test is not yet implemented");
+
+            using var sourceVsrStream = TestHelper.GetVsrStream();
+            using var sourceReader = new BinaryReader(sourceVsrStream);
+
+            var serializer = ServiceFactory.CreateVsrLevelPackSerializer();
+            var (deserializedVsr, deserializedLevelPack) = serializer.Deserialize(sourceReader, (new Models.Vsr(), new Models.LevelPack()));
+
+            using var writer = new BinaryWriter(new MemoryStream());
+
+            serializer.Serialize((deserializedVsr, deserializedLevelPack), writer);
+
+            writer.BaseStream.Position = 0;
+            var debugOutput = File.OpenWrite(Path.Combine(TestHelper.GetFixturePath(), "debug.vsr"));
+            writer.BaseStream.CopyTo(debugOutput);
+
+            _ = writer.BaseStream.Length.Should().Be(sourceVsrStream.Length, "The serialized VSR should have the same length as the original VSR");
+
+            sourceVsrStream.Position = 0;
+            writer.BaseStream.Position = 0;
+
+            for (var i = 0; i < sourceVsrStream.Length; i++)
+            {
+                var expectedValue = sourceVsrStream.ReadByte();
+                var actualValue = writer.BaseStream.ReadByte();
+
+                _ = actualValue.Should().Be(expectedValue, $"Expected byte address {i} to be {expectedValue}, not {actualValue}");
+            }
+        }
+    }
+}
