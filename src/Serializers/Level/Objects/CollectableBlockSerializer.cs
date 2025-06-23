edef9c33076fa6cb2882cd7a4c5c7a04bda97009
lajohnston <lajohnston@live.co.uk>
2025-06-23 20:28:02 +0100

    Add flag serialization (no tests yet)

--- src/Serializers/Level/Objects/CollectableBlockSerializer.cs
+++ src/Serializers/Level/Objects/CollectableBlockSerializer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using LemballEditor.Models.LevelObjects;
 
 namespace LemballEditor.Serializers.Level.Objects
@@ -9,6 +11,13 @@ namespace LemballEditor.Serializers.Level.Objects
         private readonly Func<Position, Flag> createFlag;
 
         private readonly ISerializer<Position> positionSerializer;
+
+        private static readonly Dictionary<Type, ushort> TypeToIdMap = new Dictionary<Type, ushort>
+        {
+            { typeof(Flag), 12 }
+        };
+
+        private static readonly Dictionary<ushort, Type> IdToTypeMap = TypeToIdMap.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
 
         public CollectableBlockSerializer(ISerializer<Position> positionSerializer, Func<Position, Flag> createFlag)
         {
@@ -22,29 +31,57 @@ namespace LemballEditor.Serializers.Level.Objects
 
             for (var i = 0; i < itemCount; i++)
             {
-                var id = reader.ReadUInt16(); // item ID
-                var type = reader.ReadUInt16();
+                var id = reader.ReadUInt16();
+                var typeId = reader.ReadUInt16();
                 var position = this.positionSerializer.Deserialize(reader, null);
-                _ = reader.ReadUInt16(); // padding
+                _ = reader.ReadUInt16(); // skip padding
 
-                switch (type)
+                var type = IdToTypeMap.TryGetValue(typeId, out var foundType)
+                    ? foundType
+                    : null;
+
+                if (type == typeof(Flag))
                 {
-                    case 12:
-                        var flag = this.createFlag(position);
-                        list.Add(flag, id);
-                        break;
-                    default:
-                        var address = reader.BaseStream.Position - 6;
-                        throw new NotSupportedException($"Unspported item type {type} at position {address}");
+                    var flag = this.createFlag(position);
+                    list.Add(flag, id);
+                }
+                else
+                {
+                    var address = reader.BaseStream.Position - 6;
+                    throw new NotSupportedException($"Unsupported item type {type} at position {address}");
                 }
             }
 
             return list;
         }
 
-        public void Serialize(PendingObjectList model, BinaryWriter writer)
+        public void Serialize(PendingObjectList list, BinaryWriter writer)
         {
-            throw new NotImplementedException("Item block serialization is not implemented yet.");
+            var collectables = list.GetObjectsOfTypes(typeof(Flag)).ToArray();
+
+            writer.Write((ushort)collectables.Length);
+
+            foreach (var collectable in collectables)
+            {
+                if (collectable.Id == null)
+                {
+                    throw new InvalidOperationException("Collectable must have an ID assigned before serialization.");
+                }
+
+                var typeId = this.GetTypeId(collectable.LevelObject);
+
+                writer.Write((ushort)collectable.Id);
+                writer.Write(typeId);
+                this.positionSerializer.Serialize(collectable.LevelObject.GetPosition(), writer);
+                writer.Write((ushort)0); // padding
+            }
+        }
+
+        private ushort GetTypeId(ILevelObject levelObject)
+        {
+            return TypeToIdMap.TryGetValue(levelObject.GetType(), out var id)
+                ? id
+                : throw new NotSupportedException($"Unsupported collectable type: {levelObject.GetType().Name}");
         }
     }
 }
