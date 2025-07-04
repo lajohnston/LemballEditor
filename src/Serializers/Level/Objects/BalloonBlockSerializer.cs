35a3008aef99d02ea78b58929c0a0d6c985c345c
lajohnston <lajohnston@live.co.uk>
2025-07-04 18:48:38 +0100

    Skip unknown bytes in Balloon block

--- src/Serializers/Level/Objects/BalloonBlockSerializer.cs
+++ src/Serializers/Level/Objects/BalloonBlockSerializer.cs
@@ -9,9 +9,14 @@ namespace LemballEditor.Serializers.Level.Objects
         {
             var unknown = reader.ReadUInt16(); // Read unknown value (related to balloon post count)
 
-            return unknown != 0
-                ? throw new NotSupportedException($"Unsupported balloon block value: {unknown} as position {reader.BaseStream.Position - 2}")
-                : model;
+            if (unknown != 0)
+            {
+                throw new NotSupportedException($"Unsupported balloon block value: {unknown} as position {reader.BaseStream.Position - 2}");
+            }
+
+            _ = reader.ReadBytes(24); // Skip 24 bytes of unknown data
+
+            return model;
         }
 
         public void Serialize(PendingObjectList model, BinaryWriter writer)
